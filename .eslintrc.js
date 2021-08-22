59c119a7671ddd5f5a021bf9b6eb3a636f0c9596
Foo <34515228+fooooooooooooooo@users.noreply.github.com>
2021-08-21 19:29:23 -0700

    stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
   },
   rules: {
     'no-restricted-syntax': 'off',
+    'no-console': 'off',
   },
 };
--- src/static/preload.js
+++ src/static/preload.js
@@ -1,14 +1,3 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  // eslint-disable-next-line no-console
   console.log('hey :)');
-  // const replaceText = (selector, text) => {
-  //   const element = document.getElementById(selector);
-  //   if (element) element.innerText = text;
-  // };
-
-  // for (const type of ['chrome', 'node', 'electron']) {
-  //   replaceText(`${type}-version`, process.versions[type]);
-  // }
 });
