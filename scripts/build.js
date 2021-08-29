e82447e280108e5a2abbd8f8a0ae92118f9053ab
Foo <34515228+fooooooooooooooo@users.noreply.github.com>
2021-08-28 23:00:18 -0700

    refactor: add missing types

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const {build} = require('vite');
-const {dirname} = require('path');
+const { build } = require('vite');
+const { dirname } = require('path');
 
-/** @type 'production' | 'development' | 'test' */
-const mode = process.env.MODE = process.env.MODE || 'production';
+/** @type {'production' | 'development' | 'test'} */
+const mode = (process.env.MODE = process.env.MODE || 'production');
 
 const packagesConfigs = [
   'packages/main/vite.config.js',
@@ -11,18 +11,17 @@ const packagesConfigs = [
   'packages/renderer/vite.config.js',
 ];
 
-
 /**
  * Run `vite build` for config file
  */
-const buildByConfig = (configFile) => build({configFile, mode});
+const buildByConfig = (/** @type {string} */ configFile) =>
+  build({ configFile, mode });
 (async () => {
   try {
     const totalTimeLabel = 'Total bundling time';
     console.time(totalTimeLabel);
 
     for (const packageConfigPath of packagesConfigs) {
-
       const consoleGroupName = `${dirname(packageConfigPath)}/`;
       console.group(consoleGroupName);
 
